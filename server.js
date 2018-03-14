fcce98c747c0d7bb8c1beeaf473bbf24e0c72530
B Hicks <b@audiblePi.com>
2018-03-13 22:37:32 -0400

    added openshift support

--- server.js
+++ server.js
@@ -17,9 +17,17 @@ runSwitchStream();
 app.get('/', (req, res) => res.json({ message: 'telemetry data stream demo' }) );
 app.get('/api/telemetry/recent', (req, res) => res.json(telemetryData)); //numerical data from spacesuit sensors
 app.get('/api/switch/recent', (req, res) => res.json(switchData)); //telemetry switches driven by numerical data points or other triggers
-http.listen(3000, () => console.log('listening on *:3000') );
+//http.listen(3000, () => console.log('listening on *:3000') );
 
 
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 
 
